742c2a50bee2c7e071c7f22c40c0117aab1d5f92
Kohei Nozaki <lbtc-xxx@bridge9.sakura.ne.jp>
2015-03-20 14:33:03 +0900

    initial

---
+++ persistence/src/main/java/persistence/SomeEntity.java
@@ -0,0 +1,15 @@
+package persistence;
+
+import java.io.Serializable;
+
+public class SomeEntity implements Serializable {
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
